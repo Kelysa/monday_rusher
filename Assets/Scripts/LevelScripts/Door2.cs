3a4c38334fda4db08bd1f9d6b7b730249d20072d
Kelysa <julie.siahou@epita.fr>
2017-02-11 23:15:27 +0100

    =Construction de la map + creation du personnage , animation et mouvement + celle de l'ia (il faut mieux coder l'ia car la mienne bug)

---
+++ Assets/Scripts/LevelScripts/Door2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2 : MonoBehaviour {
+
+    public float speed;
+    public bool opening = false;
+    public float maxOpenValue;
+    private float currentValue = 0;
+    public Transform door;
+    public bool closing = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (opening) OpenDoor();
+        if (closing) closeDoor();
+
+    }
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.transform.name == "Player")
+        {
+            opening = true;
+            closing = false;
+        }
+    }
+    void OnTriggerExit(Collider obj)
+    {
+        if (obj.transform.name == "Player")
+        {
+            opening = false;
+            closing = true;
+        }
+    }
+    void OpenDoor()
+    {
+        float movement = speed * Time.deltaTime;
+        currentValue += movement;
+        if (currentValue <= maxOpenValue)
+        {
+            door.position = new Vector3(door.position.x , door.position.y, door.position.z - movement);
+        }
+        else
+        {
+            opening = false;
+
+        }
+
+    }
+    void closeDoor()
+    {
+        float movement = speed * Time.deltaTime;
+        currentValue -= movement;
+        if (currentValue >= 0)
+        {
+            door.position = new Vector3(door.position.x, door.position.y, door.position.z + movement);
+        }
+        else
+        {
+            closing = false;
+
+        }
+    }
+}
